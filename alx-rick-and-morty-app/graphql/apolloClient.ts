d2435b1bec3f73c5b49efd211135635775d11a63
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-10-19 23:59:01 +0100

    Update apolloClient.ts

--- alx-rick-and-morty-app/graphql/apolloClient.ts
+++ alx-rick-and-morty-app/graphql/apolloClient.ts
@@ -1,1 +1,13 @@
 
+```ts
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://rickandmortyapi.com/graphql"
+  }),
+  cache: new InMemoryCache()
+});
+
+export default client;
+
